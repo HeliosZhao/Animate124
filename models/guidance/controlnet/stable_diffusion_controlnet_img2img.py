9cdbd69c925be06c8f03b8ae58d95975ac1a9b34
HeliosZhao <yuyangzhao@outlook.com>
2023-12-24 00:17:14 +0800

    update import aligned with newest diffusers

--- models/guidance/controlnet/stable_diffusion_controlnet_img2img.py
+++ models/guidance/controlnet/stable_diffusion_controlnet_img2img.py
@@ -10,15 +10,15 @@ from transformers import CLIPImageProcessor, CLIPTextModel, CLIPTokenizer
 
 from diffusers import AutoencoderKL, ControlNetModel, DiffusionPipeline, UNet2DConditionModel, logging
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput, StableDiffusionSafetyChecker
-from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion_controlnet import MultiControlNetModel
+from diffusers.pipelines.controlnet.multicontrolnet import MultiControlNetModel
 from diffusers.schedulers import KarrasDiffusionSchedulers
 from diffusers.utils import (
     PIL_INTERPOLATION,
     is_accelerate_available,
     is_accelerate_version,
-    randn_tensor,
     replace_example_docstring,
 )
+from diffusers.utils.torch_utils import randn_tensor
 
 from diffusers.loaders import TextualInversionLoaderMixin, LoraLoaderMixin
 
